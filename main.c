4a6771fd9aa4f33bffd99d5502400eb149c3e558
Noons-ledev <noah.briet@icloud.com>
2025-01-06 14:03:39 +0100

    Adding main.c file and checking betty

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+*main- Entry of program
+*@argc: Argument count
+*@argv: array of arguments
+*Return: Always 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	if (isatty(STDIN_FILENO))
+	exec_loop();
+	else
+	exec_no_loop();
+	return (0);
+}
