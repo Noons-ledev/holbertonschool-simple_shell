520c186721e88e1ff497c5524afcc0f62a3790bf
Noons-ledev <noah.briet@icloud.com>
2025-01-06 15:14:55 +0100

    First try on Shell version without adjusting function.c file

---
+++ functions.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+extern char **environ;
+/**
+*_getenv-get the value of an environment variable
+*@name: Name of the variable to get its value
+*Return: Pointer to the value of the variable
+*/
+char *_getenv(const char *name)
+{
+int i = 0;
+size_t name_len = strlen(name);
+if (name == NULL || environ == NULL)
+	{
+	return NULL;
+	}
+	while	(environ[i])
+		{
+	if (strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
+		{
+		return environ[i] + name_len + 1;
+		}
+		i++;
+		}
+		return NULL;
+}
+/**
+*split_string: Splits a string in elements of an array
+*@str: String to split
+*Return: array of pointers (pointer to pointer)
+*/
+char **split_string(char *str)
+{
+char **tokens_array;
+char *token;
+int i = 0, j = 0, words_number = 0;
+	for (; str[j]; j++)
+	{
+		if (str[j] != ' ' && (j != 0 || str[j - 1] == ' '))
+		{
+			words_number++;
+		}
+	}
+tokens_array = malloc(words_number * sizeof(char *));
+	if (tokens_array == NULL)
+	return (NULL);
+token = strtok(str, " ");
+	while (token)
+	{
+		tokens_array[i] = token;
+		token = strtok(NULL, " ");
+		i++;
+	}
+return (tokens_array);
+}
+/**
+*exec_loop- executes the prompt for interacting with the process
+*Return: 0 for success
+*/
+int exec_loop(void)
+{
+char *command = NULL;
+size_t len = 0;
+int id, status;
+while(1)
+{
+printf("#cisfun$ ");
+
+if (getline(&command, &len, stdin) > 0)
+	{
+	remove_newline(command);
+	if (strcmp(command, "exit") == 0)
+	break;
+	exec_command(command);
+	}
+else
+break;
+}
+free(command);
+return (0);
+}
+
+void remove_newline(char *str) 
+{
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n') 
+	{
+        str[len - 1] = '\0';
+    }
+}
+
+void exec_command(char *command)
+{
+char **tokens;
+int id, status;
+tokens = split_string(command);
+	if (tokens == NULL)
+	{
+		return;
+	}
+id = fork();
+	if (id != 0)
+	{
+		wait(&status);
+	}
+else 
+	{
+	if (execve(tokens[0], tokens, NULL) == -1)
+	{
+		printf("./shell No such file or directory\n"); 
+	}
+	exit(EXIT_FAILURE);
+}
+free(tokens);
+}
+int exec_no_loop(void)
+{
+char *command = NULL;
+size_t len = 0;
+if (getline(&command, &len, stdin) > 0)
+	{
+	remove_newline(command);
+	if (strcmp(command, "exit") == 0)
+	{
+	free(command);
+	exit(0);
+	}
+	exec_command(command);
+	}
+else
+perror("message d'erreur");
+free(command);
+return (0);
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+void remove_newline(char *str);
+char **split_string(char *str);
+int comm_exec(char **command);
+char *find_command_path(char *command);
+char *_getenv(const char *name);
+int exec_loop(void);
+int exec_no_loop(void);
+void exec_command(char *command);
+
+#endif
+
